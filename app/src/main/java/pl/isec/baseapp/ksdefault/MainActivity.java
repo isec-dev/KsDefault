63bd09abe463dda7b363096cb44483ceac8b6eb2
Kamil Kubacka <kamilkubacka+git@gmail.com>
2023-08-31 21:15:12 +0200

    Init

---
+++ app/src/main/java/pl/isec/baseapp/ksdefault/MainActivity.java
@@ -0,0 +1,141 @@
+package pl.isec.baseapp.ksdefault;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.security.crypto.MasterKeys;
+import android.os.Bundle;
+import android.security.keystore.KeyInfo;
+import android.security.keystore.KeyProperties;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String ANDROID_KEYSTORE = "AndroidKeyStore";
+    private final static String FILENAME = "encrypted.txt";
+
+    private String mSecurityLevel;
+    private SecureFile mSecureFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText outputText = findViewById(R.id.editTextTextMultiLine);
+        EditText inputText = findViewById(R.id.editTextTextMultiLine2);
+        Button readButton = findViewById(R.id.button);
+        Button saveButton = findViewById(R.id.button2);
+        TextView securityLevelText = findViewById(R.id.textView3);
+
+        try {
+            initialize();
+        } catch (GeneralSecurityException | IOException e){
+            e.printStackTrace();
+            notify("Something gone wrong!");
+        }
+
+        securityLevelText.setText(mSecurityLevel);
+
+        readButton.setOnClickListener(view ->{
+            mSecureFile.openFileInput(new SecureFile.OpenFileInputCallback() {
+                @Override
+                public void onError(@NonNull CharSequence errString) {
+                    MainActivity.this.notify("Authentication failed!");
+                    outputText.setText("#" + errString);
+                }
+
+                @Override
+                public void onInputStreamReady(@NonNull InputStream inputStream) {
+                    try {
+                        // Must be compatible with EncryptedFile.openFileInput(), see: https://developer.android.com/topic/security/data#read-files
+                        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                        int nextByte = inputStream.read();
+                        while (nextByte != -1) {
+                            byteArrayOutputStream.write(nextByte);
+                            nextByte = inputStream.read();
+                        }
+                        outputText.setText(
+                            byteArrayOutputStream.toString("UTF-8")
+                        );
+                    } catch(IOException e){
+                        e.printStackTrace();
+                        MainActivity.this.notify("Reading failed!");
+                    }
+                }
+            });
+        });
+
+        saveButton.setOnClickListener(view ->{
+            mSecureFile.openFileOutput(new SecureFile.OpenFileOutputCallback() {
+                @Override
+                public void onError(@NonNull CharSequence errString) {
+                    MainActivity.this.notify("Authentication failed!");
+                    outputText.setText("#" + errString);
+                }
+
+                @Override
+                public void onOutputStreamReady(@NonNull OutputStream outputStream) {
+                    try {
+                        // Must be compatible with EncryptedFile.openFileOutput(), see: https://developer.android.com/topic/security/data#write-files
+                        outputStream.write(
+                            inputText.getText().toString().getBytes(UTF_8)
+                        );
+                        outputStream.flush();
+                        outputStream.close();
+                        inputText.setText("");
+                    } catch(IOException e){
+                        e.printStackTrace();
+                        MainActivity.this.notify("Writing failed!");
+                    }
+                }
+            });
+        });
+    }
+
+    private void notify(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    private void initialize() throws GeneralSecurityException, IOException {
+        /** Get or create key **/
+        String masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC);
+
+        /** Get security level **/
+        KeyStore ks = KeyStore.getInstance(ANDROID_KEYSTORE);
+        ks.load(null);
+        SecretKey secretKey = (SecretKey) ks.getKey(masterKeyAlias, null);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance(secretKey.getAlgorithm(), ANDROID_KEYSTORE);
+        KeyInfo keyInfo = (KeyInfo) factory.getKeySpec(secretKey, KeyInfo.class);
+
+        switch(keyInfo.getSecurityLevel()){
+            case KeyProperties.SECURITY_LEVEL_SOFTWARE:
+                mSecurityLevel = "Software"; break;
+            case KeyProperties.SECURITY_LEVEL_TRUSTED_ENVIRONMENT:
+                mSecurityLevel = "TEE"; break;
+            case KeyProperties.SECURITY_LEVEL_STRONGBOX:
+                mSecurityLevel = "StrongBox"; break;
+            default:
+                mSecurityLevel = "Unknown";
+        }
+
+        /** Create instance of the SecureFile **/
+        mSecureFile = new SecureFileImpl(
+                new File(getFilesDir(), FILENAME),
+                this,
+                masterKeyAlias
+        );
+    }
+}
